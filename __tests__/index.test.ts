df27f8d0854fdfaafc4e3a88b910b34f052e38d5
Clyde D'Souza <clyd94@gmail.com>
2023-07-07 22:13:20 +1200

    Add tests for cli tool

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,9 +1,63 @@
-describe("lint", () => {
-    it("alphabets supplied", () => {
-        expect(true).toBeTruthy()
-    });  
+import { prepareEnvironment } from '@gmrchk/cli-testing-library';
 
-    it("alphabets suppliedsssss", () => {
-        expect(true).toBeTruthy()
-    });   
+// https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+const SEMVER_REGEX = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/
+const MARKDOWN_LINT_CLI_TOOL = './lib/index.min.js'
+
+let execute: any, cleanup: any
+
+describe("markdown-list-linter CLI tool", () => {
+    beforeEach(async () => {
+        const cliTestingLibrary = await prepareEnvironment();
+        execute = cliTestingLibrary.execute
+        cleanup = cliTestingLibrary.cleanup
+    })
+
+    afterEach(async () => {
+        await cleanup();
+    })
+
+    it('should display help text', async () => {
+        const { code, stdout } = await execute(
+            'node',
+            `${MARKDOWN_LINT_CLI_TOOL} -h`
+        );
+
+        const helpText = [
+            'Usage: markdown-list-linter [options]',
+            'Tbc',
+            'Options:',
+            '-V, --version        output the version number',
+            '-f, --file  [value]  path to the markdown file that needs to be linted',
+            '-h, --help           display help for command'
+            ]
+        
+        expect(code).toBe(0);
+        expect(stdout).toMatchObject(helpText)
+    });
+
+    it('should display version number', async () => {
+        const { code, stdout } = await execute(
+            'node',
+            `${MARKDOWN_LINT_CLI_TOOL} -V`
+        );
+        
+        expect(code).toBe(0);
+        expect(stdout[0].toString().match(SEMVER_REGEX)).toBeTruthy()
+    });
+
+    it('should display no errors found when valid file is passed', async () => {
+        const { code, stdout } = await execute(
+            'node',
+            `${MARKDOWN_LINT_CLI_TOOL} -f ./data/basic_pass.md`
+        );
+
+        const expectedOutput = [
+            'SUMMARY:',
+            'No errors found',
+            ]
+        
+        expect(code).toBe(0);
+        expect(stdout).toMatchObject(expectedOutput)
+    });
 });
