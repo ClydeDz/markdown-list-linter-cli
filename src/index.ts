f1724d72796dbaad9db36fb57159dde92aea415b
Clyde D'Souza <clyd94@gmail.com>
2023-07-07 23:21:26 +1200

    Use output string builder and update tests

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,44 @@
 #! /usr/bin/env node
 
-import { lintMarkdownList } from "markdown-list-linter";
-import { Command } from "commander";
+import { lintMarkdownList } from "markdown-list-linter"
+import { Command } from "commander"
 
-const program = new Command();
+const program = new Command()
 
 program
   .name('markdown-list-linter')
   .version("0.0.5")
   .description("CLI tool to lint markdown lists to warn when list items are not alphabetically ordered")
   .option("-f, --file  [value]", "path to the markdown file that needs to be linted")
-  .parse(process.argv);
+  .parse(process.argv)
 
-const options = program.opts();
+const options = program.opts()
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp();
+  program.outputHelp()
 }
 
 if (options.file) {
-  const filepath = typeof options.file === "string" ? options.file : __dirname;
+  const filepath = typeof options.file === "string" ? options.file : __dirname
   const result = lintMarkdownList(filepath)
-  console.log('SUMMARY:')
-  console.log(result.summary)
-  console.log()
-  result.errorObject ? console.log('DETAILS:') : undefined
+
+  let outputBuilder = ''  
+  outputBuilder += 'SUMMARY:\n' + result.summary + '\n'  
+  result.errorObject ? outputBuilder += '\nDETAILS:\n' : undefined
+
   result.errorObject?.forEach(error => {
-    console.log(error.message)
-    error.details.forEach(errorSections => {      
+    outputBuilder += error.message + '\n'
+    
+    error.details.forEach((errorSections, index) => {      
+      outputBuilder +=  '\tSection #' + (index + 1) + '\n'
+
       errorSections.forEach(errorItem => {
-        console.log('\t' + errorItem)
+        outputBuilder +=  '\t\t' + errorItem + '\n'
       })
-      console.log()
-    })    
-    console.log()
-  })  
+
+      outputBuilder += '\n'
+    })
+  })
+
+  console.log(outputBuilder)
 }
